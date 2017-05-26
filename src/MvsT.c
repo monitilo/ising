2715594ff5bbc7a3272a509d164d6f92966bcab8
German Agustin Chiarelli <gchiarelli@ws17.labo5>
2017-05-26 15:14:03 -0300

    segmentation fault en redes chicas

--- src/MvsT.c
+++ src/MvsT.c
@@ -8,64 +8,49 @@
 
 int main(int argc, char **argv) {
 
-  int n = 8;
+  int n = 20;
   float prob = 0.5;
   float T = 0;
-  int niter = 2000;
+  int niter = 500000;
   float J=1, B=0;
   float e ;
   int m;
   FILE *fs;
-  int qiter=100;
+
   srand(time(NULL));
 
   for (int w=0 ; w<100 ; w++){
-    T = 0.04 *(w+1) ;
 
-    float *evector = malloc(qiter*sizeof(float));
-    float *mvector = malloc(qiter*sizeof(float));
+    T = 0.04 *(w+1) ;
 
     printf("L = %d; T = %f; Z = %d\n",n,T,niter);
 
 
-
-    for(int q=0; q<qiter ;q++){
-
-      int *lattice = malloc(n * n * sizeof(int));
+    int *lattice = malloc(n * n * sizeof(int));
 
 
-      fill_lattice(lattice, n, prob);
+    fill_lattice(lattice, n, prob);
 
-      float *expo = malloc(10*sizeof(float));
+    float *expo = malloc(10*sizeof(float));
 
-      for (int i=-2; i<3; i++) expo[i+2]  = exp(-(J*4*i+2*B)/T) ;
-      for (int i=-2; i<3; i++) expo[i+2+5]= exp(-(J*4*i-2*B)/T) ;
-
-
-      e = energia(lattice,n,J,B);
-      m = magnetizacion(lattice,n);
+    for (int i=-2; i<3; i++) expo[i+2]  = exp(-(J*4*i+2*B)/T) ;
+    for (int i=-2; i<3; i++) expo[i+2+5]= exp(-(J*4*i-2*B)/T) ;
 
 
-      for (int i = 0; i < niter; i++) {
-        metropolis(lattice, n, J, B, expo, &e, &m);
-
-      }
-      evector[q] = e;
-      mvector[q] = (float) m;
+    e = energia(lattice,n,J,B);
+    m = magnetizacion(lattice,n);
 
 
-      free(lattice);
-      free(expo);
+    for (int i = 0; i < niter; i++) metropolis(lattice, n, J, B, expo, &e, &m);
 
-    }
+
 
     fs= fopen ("MvsT.txt", "a");
-    fprintf(fs,"%f %f %f\n" , T, esperanza(evector,qiter), esperanza(mvector,qiter));
+    fprintf(fs,"%f %f %d\n" , T, e, m);
     fclose(fs);
 
-    free(evector);
-    free(mvector);
-
+    free(lattice);
+    free(expo);
 
   }
   return 0;
