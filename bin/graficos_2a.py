c919fb387c13262609cdc40add82f0251157bae8
Facundo Luciano Sanchez <flsanchez94@gmail.com>
2017-06-05 22:04:56 -0300

    ahora graficos_2a.py toma del txt el valor del campo para graficar el teorico

--- bin/graficos_2a.py
+++ bin/graficos_2a.py
@@ -4,11 +4,19 @@ import sys
 
 T, E , sE, M , sM= np.loadtxt(sys.argv[1],unpack=True)
 
+f=open(sys.argv[1],'r')
+aux=f.readline()
+f.close()
+aux=aux.split(" ")
+aux=aux[1]
+aux=aux.split("=")
+B=float(aux[1])
+
 plt.figure(1)
 
 plt.subplot(211)
 plt.plot(T,M,'.-b')
-plt.plot(np.trim_zeros(T),32**2*np.tanh(0.001/np.trim_zeros(T)),'.-r')
+plt.plot(np.trim_zeros(T),32**2*np.tanh(B/np.trim_zeros(T)),'.-r')
 plt.subplot(212)
 plt.plot(T,sM,'.-b')
 plt.show()
